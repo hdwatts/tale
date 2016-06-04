0be438a7da66f05d801d20bbb2a8f9f92b89b300
alexseth1124 <alexseth1124@gmail.com>
2016-06-04 18:12:04 -0400

    redirects to user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,10 @@ class SessionsController < ApplicationController
   end
   
   def create
-    binding.pry
     @user = User.find_by(username: params[:user][:username])
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to welcome_path
+      redirect_to @user
     else
       redirect_to root, notice: "Invalid"
     end
