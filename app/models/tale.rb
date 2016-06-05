1c7ddb37651958916c80b3fda6c2c754655bcba8
hdwatts <howard@hdwatts.com>
2016-06-05 16:44:56 -0400

    Added seed

--- app/models/tale.rb
+++ app/models/tale.rb
@@ -1,5 +1,5 @@
 class Tale < ApplicationRecord
-  belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
   has_many :lines
   has_many :users, through: :lines
   has_many :tags_tales
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dean = User.create(first_name: "Dean", last_name: "Watts", username: "hdwatts", email: "dean@tales.com", password: "password1" )
+alex = User.create(first_name: "Alex", last_name: "Martin", username: "asmartin", email: "alex@tales.com", password: "password1" )
+eve = User.create(first_name: "Eve", last_name: "Essex", username: "eessex", email: "eve@tales.com", password: "password1" )
+
+5.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  username = Faker::Hipster.word until username && username.length > 6
+  email = first_name.downcase + "@flatironschool.com"
+  password = Faker::Hipster.word until password && password.length > 6
+  user = User.create!(first_name: first_name, username: username, last_name: last_name, email: email, password: password) unless User.all.any?{|user| user.username == username}
+  [rand(6).years, rand(365).days, rand(24).hours, rand(60).minutes, rand(60).seconds].each{|time| user.created_at -= time}
+  user.save!
+end
+
+5.times do
+  user = User.all.sample
+  t = Tale.create(prompt: Faker::Hipster.paragraph(rand(3)), owner: user)
+  t.save!
+  content = Faker::Hipster.paragraph until content && content.length <= 250
+  first_line = Line.create(user: user, tale: t, content: content)
+end
+
+Tale.all.each do |tale|
+  (rand(4)+1).times do
+    user = User.all.sample until user != Tale.first.lines.last
+    content = Faker::Hipster.paragraph until content && content.length <= 250
+    Line.create(user: user, tale: tale, content: content)
+  end
+end
+
+10.times do
+  tag = Tag.create(name: Faker::Hipster.word)
+  (rand(Tale.all.size) + 1).times do
+    tale = Tale.all.sample until tale && !tag.tales.include?(tale)
+    TagsTale.create(tag: tag, tale: tale )
+    tale.save
+  end
+end
+
+# User.all.each{|user| User.all.shuffle.first(rand(10)).each{|followee| user.follow(followee)}; user.save}
+
+# Bar.all.each do |bar|
+#  User.all.shuffle.first(rand(11)).each do |user|
+#    rating = rand(5) + 1
+#    Review.create(bar: bar, user: user, rating: rating, content: Faker::Hipster.paragraph(rand(5) + 1))
+#  end
+# end
+# User.all.each {|user| 150.times {Like.create(user: user, review: Review.find(rand(1..10000)))}}
