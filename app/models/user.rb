4c220e990da489e11329d42216275aecf1836384
alexseth1124 <alexseth1124@gmail.com>
2016-06-05 16:52:26 -0400

    can see tales on user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,18 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :first_name, :last_name, :username, :email
   validates_uniqueness_of :username, :email
-  # validate :valid_email
+  validate :valid_email
+  validate :minimum_six_character_password
 
-  # def valid_email
-  #   errors.add(:email, "is invalid") unless 
-  # end
+  def valid_email
+    errors.add(:email, "is invalid") if email !~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  end
+
+  def minimum_six_character_password
+    errors.add(:password, "is invalid") if password.length < 6
+  end
+
+ 
+
+
 end
