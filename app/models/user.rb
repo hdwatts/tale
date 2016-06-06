604e028493b6e5042cd397b9a394215efbb6365f
alexseth1124 <alexseth1124@gmail.com>
2016-06-06 12:09:54 -0400

    forgot to change user back

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :username, :email
   validates_uniqueness_of :username, :email
   validate :valid_email
-  # validate :minimum_six_character_password
+  validate :minimum_six_character_password
 
   def valid_email
     errors.add(:email, "is invalid") if email !~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   end
 
-  # def minimum_six_character_password
-  #   errors.add(:password, "is invalid") if password.length < 6
-  # end
+  def minimum_six_character_password
+    errors.add(:password, "is invalid") if password.length < 6
+  end
 
   def tales_owned
     tales.where(owner_id: id)
