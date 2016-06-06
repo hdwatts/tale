4362290ef7214481183e4d3f11072f8306e7e122
hdwatts <howard@hdwatts.com>
2016-06-06 14:24:50 -0400

    Fixed first tale bug

--- app/services/tale_creator.rb
+++ app/services/tale_creator.rb
@@ -3,6 +3,7 @@ class TaleCreator
   def self.create_tale(tale_params, current_user)
     @tale = Tale.new(tale_params)
     @tale.lines.first.user = current_user
+    @tale.lines.first.done = true
     @tale.owner = current_user
     @tale
   end
