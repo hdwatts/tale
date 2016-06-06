2e9ee3e6cf331b5c3dded166bf84ae9da2d038c9
Eve <eve.essex@gmail.com>
2016-06-06 11:20:58 -0400

    updated tale form to create lines

--- app/controllers/tales_controller.rb
+++ app/controllers/tales_controller.rb
@@ -2,14 +2,16 @@ class TalesController < ApplicationController
 
   def new
     @tale = Tale.new
-    @tale.lines.build
-    # @tale.tags.build
+    @line = @tale.lines.build
+    @tag = @tale.tags.build
   end
 
   def create
     @tale = Tale.new(tale_params)
-    if @tale.save
-      redirect_to tale_path(@tale)
+    @tale.lines.first.user_id = current_user.id 
+    @tale.owner_id = current_user.id
+    if @tale.save!
+      redirect_to @tale
     else
       @tale.errors.full_messages.each { |error| "#{error}" }
       render 'new'
@@ -23,7 +25,7 @@ class TalesController < ApplicationController
 private
   
   def tale_params
-    params.require(:tale).permit(:prompt, :lines_attributes[0][:content], :tags_tales[:tags])
+    params.require(:tale).permit(:prompt, :owner_id, lines_attributes: [:id, :content], tags_attributes: [:name])
   end
 
 end
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,6 +1,6 @@
 class Line < ApplicationRecord
   belongs_to :user
-  belongs_to :tale
+  belongs_to :tale, inverse_of: :lines
 
   validates :content, length: { minimum: 10, maximum: 250}
 end
