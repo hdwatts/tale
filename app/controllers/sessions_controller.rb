257c15a0971d42a77d538a15b318c77079e83a5a
alexseth1124 <alexseth1124@gmail.com>
2016-06-07 16:57:20 -0400

    sign out button works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
       render 'users/signin', error: "Invalid username or password"
     end
   end
+
+  def destroy
+    session.delete :user_id
+    redirect_to new_session_path
+  end
 end
