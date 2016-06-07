ebee2fcd7b8814dae274972c921567e1faf59bc7
hdwatts <howard@hdwatts.com>
2016-06-07 13:43:26 -0400

    fixed bug for wrong login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   
   def create
     @user = User.find_by(username: params[:user][:username])
-    if @user.authenticate(params[:user][:password])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to @user
     else
-      redirect_to root, notice: "Invalid"
+      redirect_to root_path, notice: "Invalid"
     end
   end
 end
