f9ee698640c26b37491b3b094a3c51e72b190ed9
Eve <eve.essex@gmail.com>
2016-06-08 13:43:18 -0400

    update line table

--- db/migrate/20160603174008_create_lines.rb
+++ db/migrate/20160603174008_create_lines.rb
@@ -1,8 +1,8 @@
 class CreateLines < ActiveRecord::Migration[5.0]
   def change
     create_table :lines do |t|
-      t.belongs_to :user, foreign_key: true
-      t.belongs_to :tale, foreign_key: true
+      t.integer :user_id
+      t.integer :tale_id
       t.text :content
       t.boolean :done, default: false
       t.timestamps
