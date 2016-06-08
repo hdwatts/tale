975af9d9c3805e6a97723b1ed049657ff7d3185e
alexseth1124 <alexseth1124@gmail.com>
2016-06-07 22:59:21 -0400

    the last refactor bug?

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,12 +1,10 @@
 class UserDecorator < SimpleDelegator
 
-  def full_name
-    first_name.titleize + " " + last_name.titleize
-  end
-
   def formatted_join_date
     created_at.strftime("%b %d, %Y")
   end
 
-
+  def full_name
+    first_name.titleize + " " + last_name.titleize
+  end
 end
