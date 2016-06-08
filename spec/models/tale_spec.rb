fe24efef88a7fb130ead6fe7c6077be7c7c8c439
Eve <eve.essex@gmail.com>
2016-06-08 13:18:59 -0400

    update testing

--- spec/models/tale_spec.rb
+++ spec/models/tale_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Tale, type: :model do
   context 'creates a valid tale' do
   let (:user) { User.create!(first_name: "R", last_name: "Spec", username: "rspec", email: "r@spec.com", password: "password", password_confirmation: "password") }
-  let (:tale) { Tale.create!(prompt: "Now I am telling a story let me tell you", owner_id: user.id, lines_attributes: {"0"=> {content: "this is the first line", user_id: user.id, done: true}}) }
+  let (:tale) { Tale.create!(title: "My Title", prompt: "Now I am telling a story let me tell you", owner_id: user.id, lines_attributes: {"0"=> {content: "this is the first line", user_id: user.id, done: true}}) }
 
     it 'has a prompt of at least 10 characters' do
       tale.prompt.length > 10
